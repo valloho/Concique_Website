3d788233db74da9f1a4c0794dc82dd0d9f2cbe6e
valloho <veronika.schramadei.haber@stud.fh-campuswien.ac.at>
2022-06-20 19:58:40 +0200

    just working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 
+
+
 const placeRouter = require('./api/routes/place-routes');
 
 const app = express();
