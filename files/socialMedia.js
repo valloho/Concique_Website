f408c2f0609f50efb4953bd46613f0f19e9108c1
schwalmentin <schwalm@gmx.at>
2022-06-20 19:57:16 +0200

    Header navbar debug

---
+++ files/socialMedia.js
@@ -0,0 +1,7 @@
+// Social Media Buttons
+
+let ss_btn_1 = document.querySelector(".ss_wrap_1 .ss_btn");
+
+ss_btn_1.addEventListener("click", function () {
+    this.classList.toggle("active");
+});
