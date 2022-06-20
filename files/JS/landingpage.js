af9572c3ef50cfbf278309328fbb3f768bf74b6b
valloho <veronika.schramadei.haber@stud.fh-campuswien.ac.at>
2022-06-20 15:12:40 +0200

    working on APIs

--- files/JS/landingpage.js
+++ files/JS/landingpage.js
@@ -1,2 +1,17 @@
 
+/*
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c945ed4b82msh07925460817f53ep144424jsnc53067c257bc',
+        'X-RapidAPI-Host': 'instagram-data1.p.rapidapi.com'
+    }
+};
+
+fetch('https://instagram-data1.p.rapidapi.com/followers?username=valloho', options)
+    .then(response => response.json())
+    .then(response => console.log(response['count']))
+    .catch(err => console.error(err));
+
+ */
 
