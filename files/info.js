136cf6dc813f4919286f3c35eaa1468768add2f4
valloho <veronika.schramadei.haber@stud.fh-campuswien.ac.at>
2022-06-21 21:26:39 +0200

    just working

--- files/info.js
+++ files/info.js
@@ -1,1 +1,48 @@
+class Place{}
 
+class infoBar {
+
+    addToDOM(places) {
+
+        for (const place of places){
+            this.addPlaceToDOM(Object.assign(new Place(), place));
+        }
+    }
+
+    addPlaceToDOM(place) {
+
+        let div = document.createElement("div");
+        div.id = place.id;
+        document.getElementById("title_img").append(div);
+
+        let h1 = document.createElement("h1");
+        h1.append(place.name);
+        div.append(h1);
+
+        let img = document.createElement("img");
+        img.src = place.image;
+        img.setAttribute("class", "bar_img");
+        h1.append(img);
+
+
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const infobar = new infoBar();
+
+    const response = await fetch(`/api/bar/places/`); //places route fehlt!!
+    if (!response.ok) {
+        throw new Error(`Fetch error, something went wrong. status: ${response.status}`);
+    }
+    const places = await response.json();
+    for (const place of Array.from(places).reverse()) {
+        const placesJSON = await fetch(`/api/bar/places/${place.id}`);
+        const places = await placesJSON.json();
+        infobar.addToDOM(places);
+    }
+
+
+});
