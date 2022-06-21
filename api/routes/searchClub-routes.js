7f4836af8f7463a29eadbbd8222793f105130a8c
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2022-06-21 23:38:31 +0200

    added searchClub stuff and yeah

---
+++ api/routes/searchClub-routes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const controller = require('../controllers/searchClub-controller');
+
+const routesSearchClub = Router();
+
+routesSearchClub.get('/events', controller.getEvents);
+/*
+routes.post('/club/events', controller.createPlace);
+routes.put('/club/events/:id', controller.updatePlace)
+routes.delete('/club/events/:id', controller.deletePlace)
+
+ */
+
+module.exports = routesSearchClub;
